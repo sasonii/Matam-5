eeb7fb15cf104fbc5596348cc21eb63413fa234b
sasonii <sasonshmuellameey@gmail.com>
2022-06-26 23:57:55 +0300

    first commit

---
+++ ex5.py
@@ -0,0 +1,40 @@
+import json
+import os
+
+
+
+def names_of_registered_students(input_json_path, course_name):
+    """
+    This function returns a list of the names of the students who registered for
+    the course with the name "course_name".
+
+    :param input_json_path: Path of the students database json file.
+    :param course_name: The name of the course.
+    :return: List of the names of the students.
+    """
+    pass
+
+
+def enrollment_numbers(input_json_path, output_file_path):
+    """
+    This function writes all the course names and the number of enrolled
+    student in ascending order to the output file in the given path.
+
+    :param input_json_path: Path of the students database json file.
+    :param output_file_path: Path of the output text file.
+    """
+    pass
+
+
+
+def courses_for_lecturers(json_directory_path, output_json_path):
+    """
+    This function writes the courses given by each lecturer in json format.
+
+    :param json_directory_path: Path of the semsters_data files.
+    :param output_json_path: Path of the output json file.
+    """
+    pass
+
+
+
